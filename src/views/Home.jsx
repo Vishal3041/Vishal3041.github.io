8938f52f6b059007b7470f9c2c31711fbeb1c94b
Vishal Rajeshkumar Tripathi <vishaltripathi1497@gmail.com>
2025-01-11 19:04:41 -0800

    Updated resume

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -64,7 +64,7 @@ const Home = () => {
             <div className="mt-5 sm:mt-8 sm:flex  lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
                 <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:scale-105 md:py-4 md:text-lg md:px-10"
-                  href="https://drive.google.com/file/d/1vJO6YSUEnxBueWVWSeyCLz0aRdVfIWBi/view?usp=sharing?usp=sharing" target="_blank" >
+                  href="https://drive.google.com/file/d/1kQDLmdNa1dzCv1WdmLQ247FxEHzUsmeY/view?usp=sharing" target="_blank" >
                   Resume
                   </a>
               </div>
